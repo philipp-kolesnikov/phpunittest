7ec90a7fc48dac886d5d55d4a65145e9ab435da9
Philipp Kolesnikov <kolesnikov@farpost.com>
2015-10-20 11:35:54 +1000

    created

---
+++ tests/Parallel2Test.php
@@ -0,0 +1,34 @@
+<?php
+
+class ParallelTest2 extends PHPUnit_Framework_TestCase {
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testFirst() {
+		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
+		assert(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testSecond() {
+		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
+		sleep(2);
+		assert(true);
+	}
+
+	/**
+	 * @runInSeparateProcess
+	 * @preserveGlobalState disabled
+	 */
+	public function testLast() {
+		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
+		assert(true);
+	}
+
+}
+ 

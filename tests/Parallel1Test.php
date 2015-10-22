96b17c4ef9928abef283451a8ece5b258cdb9f41
Philipp Kolesnikov <kolesnikov@farpost.com>
2015-10-22 10:27:47 +1000

    use phpunit.xml instead of annotations

--- tests/Parallel1Test.php
+++ tests/Parallel1Test.php
@@ -1,33 +1,18 @@
 <?php
 
-/**
- * @runTestsInSeparateProcesses
- */
 class ParallelTest1 extends PHPUnit_Framework_TestCase {
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testFirst() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		sleep(2);
 		assert(true);
 	}
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testSecond() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		assert(true);
 	}
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testLast() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		assert(true);
--- tests/Parallel2Test.php
+++ tests/Parallel2Test.php
@@ -2,29 +2,17 @@
 
 class ParallelTest2 extends PHPUnit_Framework_TestCase {
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testFirst() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		assert(true);
 	}
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testSecond() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		sleep(2);
 		assert(true);
 	}
 
-	/**
-	 * @runInSeparateProcess
-	 * @preserveGlobalState disabled
-	 */
 	public function testLast() {
 		print(__CLASS__."::".$this->getName()." pid is ".getmypid());
 		assert(true);
